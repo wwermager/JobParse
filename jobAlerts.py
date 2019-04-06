429c282d80316c2daacefcd83c1fec38945c24a6
wwermager <will@wermager.me>
2019-04-06 01:09:18 -0500

    Check for additional pages and scrape them

--- jobAlerts.py
+++ jobAlerts.py
@@ -1,27 +1,43 @@
 from bs4 import BeautifulSoup
 import urllib2
 import time
-
+CONST_URL="https://edpost.stcloudstate.edu/?page="
+FILTER_URL="&submit=Search+Entire+Posting&currentFilter="
 SEARCH_STRINGS=["english+teacher","language+arts","english+language+arts","language+arts+teacher"]
 jobs = []
 all_jobs = []
 
-print "----------------------------------------------------------"
+print "--------------------------------------------------------"
 
-for srchStr in SEARCH_STRINGS:
-    print "Wating 5 seconds"
-    time.sleep(5) #Slow down call rate
-    url = "https://edpost.stcloudstate.edu/?KeyWordString="+srchStr+"&StateCityZipString=&submit=Search+Entire+Posting"
-    print "Trying url:\n",url
-    html_doc = urllib2.urlopen(url)
-    soup = BeautifulSoup(html_doc,'html.parser')
-    #print(soup.prettify())
-    jobs = soup.find_all('a',{"class":"jobtitle"})
-    print "With search string ",srchStr," found ",len(jobs)," jobs."
-    print "----------------------------------------------------------"
-    for job in jobs:
-        all_jobs.append(job)
+def has_next_page(soup):
+    if len(soup.find_all('li',{"class":"PagedList-skipToNext"}))==0:
+        return False
+    else:
+        return True
 
+def job_search(search_str):
+    page_num=1
+    url = CONST_URL+str(page_num)+FILTER_URL+search_str
+    soup = BeautifulSoup(" ",'html.parser')
+    print "Searching for jobs with keywords: ",search_str
+    print "--------------------------------------------------------"
+    while has_next_page(soup) or page_num==1:
+        time.sleep(5) #Slow down call rate
+        print "Trying url:\n",url
+        html_doc = urllib2.urlopen(url)
+        soup = BeautifulSoup(html_doc,'html.parser')
+        jobs = soup.find_all('a',{"class":"jobtitle"})
+        for job in jobs:
+            all_jobs.append(job)
+        print len(jobs)," jobs on page ",page_num
+        page_num += 1
+        url=CONST_URL+str(page_num)+FILTER_URL+search_str
+    print "--------------------------------------------------------"
+    return all_jobs
+
+for keyword in SEARCH_STRINGS:
+    job_search(keyword)
+print "################################################"
 print "Total jobs found: ",len(all_jobs)
 for job in all_jobs:
     print(job.get_text())
