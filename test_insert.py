a8c7dad236f1726dcd301e303de558b358c75ead
will <will@Warmonger.localdomain>
2019-04-08 23:01:30 -0500

    removed class - made function calls

--- test_insert.py
+++ test_insert.py
@@ -1,4 +1,4 @@
-from db import Job_Post
-record = Job_Post(9999999991,"test","test",111111111111,"test","test",1)
-print record.app_id
-record.insert()
+from db import insert,close
+for x in range (1, 1000):
+    insert(x,"test","test",111111111111,"test","test",1)
+close()
